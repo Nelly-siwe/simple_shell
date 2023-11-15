1fb82bf4caecda008b62e60f7b929448cf049fbf
Nelly-siwe <nelly.fumile@gmail.com>
2023-11-15 10:48:35 -0800

    Fixing errors

--- main.c
+++ main.c
@@ -1,43 +1,43 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success, 1 on error
+ * main - start function
+ * @argcount: argument count
+ * @argvector: argument vector
+ * Return: 0=success, 1=error
  */
-int main(int ac, char **av)
+int main(int argcount, char **argvector)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+        info_t information[] = { INFO_INIT };
+        int filed = 2;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+        asm ("mov %1, %0\n\t"
+                "add $3, %0"
+                : "=r" (filed)
+                : "r" (filed));
 
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+        if (argcount == 2)
+        {
+                filed = open(argvector[1], O_RDONLY);
+                if (filed == -1)
+                {
+                        if (errno == EACCES)
+                                exit(126);
+                        if (errno == ENOENT)
+                        {
+                                _eputs(argvector[0]);
+                                _eputs(": 0: Can't open ");
+                                _eputs(argvector[1]);
+                                _eputchar('\n');
+                                _eputchar(BUF_FLUSH);
+                                exit(127);
+                        }
+                        return (EXIT_FAILURE);
+                }
+                information->readfd = filed;
+        }
+        populate_env_list(information);
+        read_history(information);
+        hsh(information, argvector);
+        return (EXIT_SUCCESS);
 }
